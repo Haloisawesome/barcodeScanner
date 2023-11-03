f6bc7ca4fa90a7c2ba8dd6587f57d51929bb8170
Haloisawesome <71446134+Haloisawesome@users.noreply.github.com>
2023-11-03 12:47:26 -0500

    Update dbh.inc.php

---
+++ dbh.inc.php
@@ -0,0 +1,11 @@
+<?php
+#dbh.inc.php
+$dsn = "mysql:host=localhost;dbname=PlumadoreTechgaloreWH";
+$dbusername = "IT391";
+$dbpassword = "pi";
+try{
+    $pdo = new PDO($dsn, $dbusername, $dbpassword);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e){
+    echo "Connection failed: " . $e->getMessage();
+}
